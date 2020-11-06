a33cd0675fd4e12fcbfcd9439e9b0fc996805a9f
abdelmawlaSouat <abdelmawla.souat@gmail.com>
2020-11-06 01:54:34 +0100

    Add Home Section

---
+++ src/components/Home.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="home my-4">
+    <v-row class="offset-md-2 col-md-10">
+      <v-col class="col-md-5">
+        <div class="line my-10"></div>
+        <h1>Humanizing your insurance.</h1>
+        <p class="my-5">
+          Get your life insurance coverage easier and faster.
+          We blend our expertise and technology to help you find
+          the plan that’s right for you. Ensure you and your
+          loved ones are protected.
+        </p>
+        <v-btn class="no-border-radius" outlined large dark>
+          View plans
+        </v-btn>
+      </v-col>
+      <v-col class="offset-md-1">
+        <v-img
+          src="@/assets/image-intro-desktop.jpg"
+          alt="Intro"
+          class="intro-img"
+        ></v-img>
+      </v-col>
+    </v-row>
+    <v-img
+      src="@/assets/bg-pattern-intro-right-desktop.svg"
+      alt="Background Right"
+      class="pattern-right"
+    ></v-img>
+    <v-img
+      src="@/assets/bg-pattern-intro-left-desktop.svg"
+      alt="Background Left"
+      class="pattern-left"
+    ></v-img>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style lang="css" scoped>
+
+.home {
+  background-color: var(--dark-violet) !important;
+  color: white;
+}
+.intro-img {
+  width: 50vh;
+  position: relative;
+  top: 18vh;
+}
+
+.line {
+  border-top: 1px solid white;
+  width: 20%;
+}
+.pattern-right {
+  position: absolute;
+  top: 12vh;
+  right: 0;
+  width: 25%;
+}
+.pattern-left {
+  position: absolute !important;
+  bottom: 0;
+  left: 0;
+  width: 15%;
+}
+h1 {
+  font-family: "DMSerif" !important;
+  font-weight: 500 !important;
+  font-size: 3.75rem;
+}
+p {
+  font-family: "Karla" !important;
+}
+
+.no-border-radius {
+  border-radius: 0 !important;
+}
+</style>
